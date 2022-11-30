61619db0706a68206e62c364e8a72b299c3b03b1
firhanabdurr <firhanabdurr@gmail.com>
2022-11-30 07:36:35 +0700

    create button detail isn't working properly

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import DetailPage from "./DetailPage";
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/Detail/:id' element={<DetailPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Pokemon from "./Pokemon";
+import HubungiKamiForm from "./HubungiKamiForm";
+class Main extends React.Component {
+    render() {
+        const listPokemon = [
+            {
+                name: "Pikachu",
+                image: "./images/pikachu.png",
+                introduction: "Pikachu adalah pokemon"
+            },
+            {
+                name: "Bulbasaur",
+                image: "#",
+                introduction: "Bulbasaur adalah pokemon"
+            },
+            {
+                name: "Blastoise",
+                image: "#",
+                introduction: "Blastoise adalah pokemon bukan gundam"
+            },
+            {
+                name: "Charmander",
+                image: "#",
+                introduction: "Charmander adalah pokemon"
+            }
+        ]
+        return (
+            <div className='main-wrapper'>
+                <div className='main'>
+                    <div className='hello-container'>
+                        <h1>Hello, Pokemon hunter</h1>
+                        <h2>Selamat datang di dunia Pokemon</h2>
+                    </div>
+                    <div className='pokemon-container'>
+                        <h3>List Pokemon</h3>
+                        <div className='list-card'>
+                            {listPokemon.map((pokemon) => {
+                                return (
+                                    <Pokemon name={pokemon.name} image={pokemon.image} introduction={pokemon.introduction} />
+                                );
+                            })}
+                        </div>
+                    </div>
+                    {/*Masukkan komponen HubungiKamiForm.js di bawah ini*/}
+                    <div className='hubungikami-container'>
+                        <h3>Hubungi Kami</h3>
+                        <HubungiKamiForm />
+                    </div>
+                </div>
+            </div>
+        );
+    };
+}
+
+export default Main;
